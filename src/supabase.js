6fc0967178f1437a9bdcc5cf51bc819933f72044
kent204921 <kwn998@gmail.com>
2025-03-29 22:16:29 +0800

    Delete src directory

--- src/supabase.js
+++
@@ -1,8 +0,0 @@
-// src/supabase.js
-import { createClient } from "@supabase/supabase-js";
-
-// 请确保只在客户端使用 anon key，不要将 service_role 密钥暴露到前端！
-const supabaseUrl = "https://oimtvnybrlwmqgsixklj.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pbXR2bnlicmx3bXFnc2l4a2xqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNjQ0NzcsImV4cCI6MjA1ODc0MDQ3N30.9NE8K6KTTmdPhuVUlf4ukTfNrGGS2F0jbFAjI6JF3yA";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
